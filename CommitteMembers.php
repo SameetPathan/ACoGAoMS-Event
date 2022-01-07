4f874b8dd8c3e5c5463464e9e345e9d9256fc43b
SameetPathan <sameetpathanrs@gmail.com>
2022-01-07 13:13:59 +0530

    Start

---
+++ CommitteMembers.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <link rel="stylesheet" href="/style/style.css">
+  <title>Committe Members</title>
+</head>
+
+<body style="font-family: Helvetica, sans-serif;">
+  <!--Navbar Start-->
+  <div>
+    <!-- Image and text -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+   <!--   <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">-->
+      <a class="navbar-brand" href="/Event/index.php">EventName</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+
+          <li class="nav-item active">
+            <a class="nav-link font" href="/Event/index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+              About Us
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="/Event/About/Conference.php">Conference</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/Event/About/Game.php">Gwalior</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/Event/About/Pccoe.php">Pccoe</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/Event/About/Pune.php">Pune</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/Scope.php">Scope</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/Paper_Submission.php">Paper Submission</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+            Registration
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+              <li><a class="dropdown-item" href="/Event/Register/ModeOfPayment.php">Mode of Payment</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/Event/Register/Register.php">Register</a></li>
+              <li>
+
+            </ul>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/CommitteMembers.php">Committee Members</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/PastEvents.php">Past Events</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/Downloads.php">Downloads</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/Event/Contact/Contact.php">Contact Us</a>
+          </li>
+
+        </ul>
+
+       <a href="/Event/Register/Register.php"> <button class="btn btn-outline-success " type="submit">Register</button></a>
+
+      </div>
+    </nav>
+
+  </div>
+  <!--Navbar End-->
+
+
+
+
+
+
+
+  <!--scripts-->
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
